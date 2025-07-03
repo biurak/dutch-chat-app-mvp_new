e2b84edbd2790b23c444a0e8902adc25683540fe
Gagik <gagik.n@gmail.com>
2025-07-03 19:11:32 +0200

    feat(layout): restructure layout component for improved HTML semantics
    * Moved <head> section inside the <html> tag for better structure.
    * Ensured <ConvexAuthNextjsServerProvider> wraps the <body> for proper context.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,16 +18,26 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<ConvexAuthNextjsServerProvider>
-			<html lang='en' className='h-full'>
+		<html lang='en' className='h-full'>
+			<head>
+				<meta charSet='UTF-8' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<meta name='theme-color' content='#ffffff' />
+				<link rel='icon' href='/favicon.ico' />
+				<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png'></link>
+				<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png'></link>
+				<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png'></link>
+				<link rel='manifest' href='/site.webmanifest'></link>
+			</head>
+			<ConvexAuthNextjsServerProvider>
 				<body className={`${inter.className} min-h-screen bg-white`}>
 					<ConvexClientProvider>
 						<Navbar />
 						{children}
 					</ConvexClientProvider>
 				</body>
-			</html>
-		</ConvexAuthNextjsServerProvider>
+			</ConvexAuthNextjsServerProvider>
+		</html>
 	)
 }
 
